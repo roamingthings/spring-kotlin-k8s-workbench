ed536797fdd30480acc3f19ebbdb4d290d27b0aa
Alexander Sparkowsky <info@roamingthings.de>
2018-03-24 16:30:02 +0100

    Convert web configuration to Kotlin class

---
+++ src/main/kotlin/de/roamingthings/kuberspring/config/WebMVConfig.kt
@@ -0,0 +1,14 @@
+package de.roamingthings.kuberspring.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebMVConfig : WebMvcConfigurer {
+
+    override fun addViewControllers(registry: ViewControllerRegistry) {
+        registry!!.addViewController("/").setViewName("index")
+    }
+
+}
