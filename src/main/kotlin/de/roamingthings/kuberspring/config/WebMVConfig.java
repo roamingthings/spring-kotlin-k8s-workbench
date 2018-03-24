0e47ff4b47c910b95ad937bd62145d7d825051e4
Alexander Sparkowsky <info@roamingthings.de>
2018-03-24 16:12:04 +0100

    Add WebMvcConfigurer interface to the configuration class

--- src/main/kotlin/de/roamingthings/kuberspring/config/WebMVConfig.java
+++ src/main/kotlin/de/roamingthings/kuberspring/config/WebMVConfig.java
@@ -2,9 +2,10 @@ package de.roamingthings.kuberspring.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMVCconfig {
+public class WebMVConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
